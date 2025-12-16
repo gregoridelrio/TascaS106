42982cdac81759f2aa3fabe1a65920ac51632cbe
Laurea Gregori <gregoridelrio@gmail.com>
2025-12-16 12:57:16 +0100

    add: añadido saneamiento de los datos

--- nivell1/exercici1/form.php
+++ nivell1/exercici1/form.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-$name = $_POST['name'];
-$surname = $_POST['surname'];
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$surname = htmlspecialchars($_POST['surname'], ENT_QUOTES, 'UTF-8');
+
 
 $_SESSION['name'] = $name;
 $_SESSION['surname'] = $surname;
 
-echo "Your name is " . $name . " " . $surname . "<br>";
+echo "Your name is $name and your surname is $surname" . "<br>";
 
 echo "Session variables: " . $_SESSION['name'] . " " . $_SESSION['surname'];
